ba0df2903d7531d428be3d6197404b680806c0d2
= <=>
2022-09-08 19:42:44 +0700

    upload code 8/9

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { Fragment } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import DefaultLayout from './components/Layout/DefaultLayout';
 function App() {
     return (
-        <div className="App">
-            <h1>Hello Tien!</h1>
-            <input></input>
-            <button>Add</button>
-        </div>
+        <BrowserRouter>
+            <div className={'App'}>
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component; // Trong JSX muốn sử dụng component dưới dạng biến thì phải viết hoa chữ cái đầu
+
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            ></Route>
+                        );
+                    })}
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
