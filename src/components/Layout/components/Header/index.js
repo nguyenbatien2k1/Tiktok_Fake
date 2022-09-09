7b34904e60d309949b5838ff994da3e1d45fdc78
= <=>
2022-09-09 18:31:01 +0700

    upload code 9/9

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,33 @@
 import classNames from 'classnames/bind';
+
 import styles from './Header.module.scss';
+import images from '~/assets/images';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
+
+console.log(images.logo);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="Tiktok" />
+                <div className={cx('search')}>
+                    <input placeholder="Tìm kiếm tài khoản và video" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
         </header>
     );
 }
